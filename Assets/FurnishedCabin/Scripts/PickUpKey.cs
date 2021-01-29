e08add5e9dbb2a8978ee423d303526f713312c15
Weact <l.druckes@ludus-academie.com>
2021-01-29 21:51:30 +0100

    some code idk

---
+++ Assets/FurnishedCabin/Scripts/PickUpKey.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpKey : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            target.GetComponent<Lock>().lockdoor = false;
+            Destroy(gameObject);
+        }
+    }
+}
