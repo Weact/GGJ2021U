550a035060762d897e3f062a12ed7dc0c87193ac
Romainmoschler <44899582+Romainmoschler@users.noreply.github.com>
2021-01-31 14:23:11 +0100

    DUDULE FLY

--- Assets/Script/TriggerZone.cs
+++ Assets/Script/TriggerZone.cs
@@ -6,14 +6,26 @@ public class TriggerZone : MonoBehaviour
 {
     public GameObject target;
     public GameObject Player;
+    public Transform Dudule;
+
+    EntityScript scriptDudule;
+
+    Vector3 diff;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            Debug.Log(Player.transform.position);
-            Player.transform.position = target.transform.position;
-            Debug.Log(Player.transform.position);
+            diff = Player.transform.position - Dudule.position;
+
+           // Debug.Log(Player.transform.position);
+           Player.transform.position = target.transform.position;
+            // Debug.Log(Player.transform.position);
+
+            Dudule.position = target.transform.position + diff;
+
+            scriptDudule.Dificulty = 30f;
+
         }
     }
 }
